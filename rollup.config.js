296eabb80af88646cea81087cf816c022f05b778
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-04 23:27:51 +0800

    chore: improve bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,18 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+import analyze from 'rollup-plugin-analyzer'
+
+export default [
+  ...createOptions({
+    directory: 'es2015'
+  , target: 'ES2015'
+  })
+, ...createOptions({
+    directory: 'es2018'
+  , target: 'ES2018'
+  })
+]
 
 function createOptions({ directory, target }) {
   return [
@@ -10,8 +22,9 @@ function createOptions({ directory, target }) {
     , output: createOutput('index')
     , plugins: [
         typescript({ target })
-      , resolve()
+      , resolve({ browser: true })
       , commonjs()
+      , analyze({ summaryOnly: true })
       ]
     }
   , {
@@ -19,7 +32,7 @@ function createOptions({ directory, target }) {
     , output: createMinification('index')
     , plugins: [
         typescript({ target })
-      , resolve()
+      , resolve({ browser: true })
       , commonjs()
       , terser()
       ]
@@ -60,14 +73,3 @@ function createOptions({ directory, target }) {
     ]
   }
 }
-
-export default [
-  ...createOptions({
-    directory: 'es2015'
-  , target: 'ES2015'
-  })
-, ...createOptions({
-    directory: 'es2018'
-  , target: 'ES2018'
-  })
-]
